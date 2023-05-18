8276de349a6cdf34896e4dbbdf14509713dae0cc
Raquel Silva Carrera <raquel.carrera@portalsesisp.org.br>
2023-05-18 09:58:38 -0300

    logica inicial

---
+++ cadastro.js
@@ -0,0 +1,64 @@
+const formulario = document.getElementById("formulario");
+const msg = document.querySelector(".mensagem")
+const nome = document.getElementById("inome");
+const email = document.getElementById("iemail");
+const senha = document.getElementById("isenha");
+
+
+function verificarEmail(email, evento){
+    let dados = JSON.parse(localStorage.getItem("bd"));
+    if (dados == null){
+        criarUsuario(evento);
+    } else {
+        dados.forEach(elemento =>{
+            if (elemento.emailcliente == email){
+                msg.innerHTML="E-mail já existe!";
+                evento.preventDefault();
+            } else {
+                criarUsuario(evento);
+            }
+        }
+        );
+    }  
+}
+
+formulario.onsubmit = (evento) =>{
+    if (nome.value == ""){
+        evento.preventDefault();
+        msg.innerHTML = "Digite seu Nome";
+        nome.focus();
+        return null;
+    }
+
+    if (email.value == ""){
+        evento.preventDefault();
+        msg.innerHTML = "Digite seu e-mail";
+        email.focus();
+        return null;
+    }
+
+    if (senha.value == ""){
+        evento.preventDefault();
+        msg.innerHTML = "Digite sua Senha!"
+        senha.focus();
+        return null;
+    }
+    verificarEmail(email.value, evento);
+}
+
+
+function criarUsuario(evento){
+    let dados = JSON.parse(localStorage.getItem("bd")) || [];
+    dados.push(
+        {
+        nomecliente : nome.value,
+        emailcliente : email.value,
+        senhacliente : senha.value
+        }
+    )
+    localStorage.setItem("bd", JSON.stringify(dados));
+    msg.innerHTML ="Usuário Cadastrado com Sucesso";
+    evento.preventDefault();
+    setTimeout(()=>{window.location.assign("login.html");},2000)
+    
+}
